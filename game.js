e284d48ac58fc682b2670e9297430ddbbf740263
armeen-fatima253 <armeenali253@gmail.com>
2024-06-27 19:39:29 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let randomnumber = Math.floor(Math.random() * 8 * 3);
+let randnum = 5;
+let ans = await inquirer.prompt([
+    {
+        name: "ali",
+        message: "plz guess a number b/w 1-5",
+        type: "number",
+    }
+]);
+if (ans.ali === randnum) {
+    console.log("congrats u guess a right num!");
+}
+else {
+    console.log("you guessed wrong!");
+}
